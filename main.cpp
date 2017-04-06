ef2591ea91f5f0f7cb58ce9f27df1c4054854797
HassanAmr <hassan.amr89@gmail.com>
2017-04-07 01:58:35 +0200

    more accurate bounding box

--- main.cpp
+++ main.cpp
@@ -106,6 +106,71 @@ static Mat drawGoodMatches(
                  good_matches, img_matches, Scalar::all(-1), Scalar::all(-1),
                  std::vector<char>(), DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS  );
 
+
+    //-- Localize the object
+    std::vector<Point2f> obj;
+    std::vector<Point2f> scene;
+
+    for( size_t i = 0; i < good_matches.size(); i++ )
+    {
+        //-- Get the keypoints from the good matches
+        obj.push_back( keypoints1[ good_matches[i].queryIdx ].pt );
+        scene.push_back( keypoints2[ good_matches[i].trainIdx ].pt );
+    }
+
+
+
+    //-- Get the corners from the image_1 ( the object to be "detected" )
+    int max_x = 0;
+    int min_x = 5000;//works as infinity for such image sizes
+    int max_y = 0;
+    int min_y = 5000;//works as infinity for such image sizes
+    int curX, curY;
+    for (size_t i = 0; i < keypoints1.size(); i++)
+    {
+        curX = keypoints1[i].pt.x;
+        curY = keypoints1[i].pt.y;
+        if (curX > max_x)
+            max_x = curX;
+        if (curY > max_y)
+            max_y = curY;
+
+        if (curX < min_x)
+            min_x = curX;
+        if (curY < min_y)
+            min_y = curY;
+    }
+
+    std::vector<Point2f> obj_corners(4);
+    obj_corners[0] = Point(min_x,min_y);
+    obj_corners[1] = Point( max_x, min_y );
+    obj_corners[2] = Point( max_x, max_y );
+    obj_corners[3] = Point( min_x, max_y );
+    std::vector<Point2f> scene_corners(4);
+
+
+    //obj_corners[0] = Point(0,0);
+    //obj_corners[1] = Point( cols, 0 );
+    //obj_corners[2] = Point( cols, rows );
+    //obj_corners[3] = Point( 0, rows );
+
+    Mat H = findHomography( obj, scene, RANSAC );
+    perspectiveTransform( obj_corners, scene_corners, H);
+
+    //-- Draw lines between the corners (the mapped object in the scene - image_2 )
+    line( img_matches,
+          scene_corners[0] + Point2f( (float)img1.cols, 0), scene_corners[1] + Point2f( (float)img1.cols, 0),
+          Scalar( 0, 255, 0), 2, LINE_AA );
+    line( img_matches,
+          scene_corners[1] + Point2f( (float)img1.cols, 0), scene_corners[2] + Point2f( (float)img1.cols, 0),
+          Scalar( 0, 255, 0), 2, LINE_AA );
+    line( img_matches,
+          scene_corners[2] + Point2f( (float)img1.cols, 0), scene_corners[3] + Point2f( (float)img1.cols, 0),
+          Scalar( 0, 255, 0), 2, LINE_AA );
+    line( img_matches,
+          scene_corners[3] + Point2f( (float)img1.cols, 0), scene_corners[0] + Point2f( (float)img1.cols, 0),
+          Scalar( 0, 255, 0), 2, LINE_AA );
+
     return img_matches;
 }
 
@@ -167,11 +232,36 @@ Mat findGoodMatches(
     if (obj.size() > 0) 
     {    
         //-- Get the corners from the image_1 ( the object to be "detected" )
-        
-        obj_corners[0] = Point(0,0);
-        obj_corners[1] = Point( cols, 0 );
-        obj_corners[2] = Point( cols, rows );
-        obj_corners[3] = Point( 0, rows );
+        int max_x = 0;
+        int min_x = 5000;//works as infinity for such image sizes
+        int max_y = 0;
+        int min_y = 5000;//works as infinity for such image sizes
+        int curX, curY;
+        for (size_t i = 0; i < keypoints1.size(); i++)
+        {
+            curX = keypoints1[i].pt.x;
+            curY = keypoints1[i].pt.y;
+            if (curX > max_x)
+                max_x = curX;
+            if (curY > max_y)
+                max_y = curY;
+
+            if (curX < min_x)
+                min_x = curX;
+            if (curY < min_y)
+                min_y = curY;
+        }
+
+        obj_corners[0] = Point(min_x,min_y);
+        obj_corners[1] = Point( max_x, min_y );
+        obj_corners[2] = Point( max_x, max_y );
+        obj_corners[3] = Point( min_x, max_y );
+
+
+        //obj_corners[0] = Point(0,0);
+        //obj_corners[1] = Point( cols, 0 );
+        //obj_corners[2] = Point( cols, rows );
+        //obj_corners[3] = Point( 0, rows );
 
         H = findHomography( obj, scene, RANSAC, 3 );
 
@@ -395,12 +485,17 @@ int main(int argc, char* argv[])
 
         currFitnessScore = (double)currMatches.size()/ (double)keypoints2.size();
         std::cout << curr_img << " -> "<< currFitnessScore << std::endl << "H = " << std::endl << currH << std::endl << std::endl;
-
-        //TODO: compute transform matrix and print 
         
         //write image to disk
         Mat img_matches = drawGoodMatches(keypoints1, keypoints2, img1.getMat(ACCESS_READ), img2.getMat(ACCESS_READ), currMatches);
         while(img_matches.empty()){};
+
+
+        //TODO: compute transform matrix and print 
+
+        //maybe do this part here to penaltilize non horizontals in drawGoodMatches??
+        //currFitnessScore = (double)currMatches.size()/ (double)keypoints2.size();
+
         currFitnessScore *= 100;
         cv::String fitnessValue = std::to_string(currFitnessScore);
         cv::String fitnessText = "Fitness: " + fitnessValue + '%';
