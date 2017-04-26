5b8c50d3cf854842f3ac023f7826d431df648b43
Hassan Gamaleldin <hassan.amr89@gmail.com>
2017-04-26 18:25:20 +0200

    removed unnecessary inlcudes

--- src/main.cpp
+++ src/main.cpp
@@ -3,15 +3,6 @@
 #include "opencv2/core.hpp"
 #include "opencv2/core/utility.hpp"
 #include "opencv2/core/ocl.hpp"
-#include "opencv2/imgcodecs.hpp"
-#include "opencv2/imgproc.hpp"
-#include "opencv2/highgui.hpp"
-#include "opencv2/features2d.hpp"
-#include "opencv2/calib3d.hpp"
-#include "opencv2/imgproc.hpp"
-#include "opencv2/xfeatures2d.hpp"
-#include "opencv2/videoio.hpp"
-#include <opencv2/video.hpp>
 
 using namespace cv;
 
